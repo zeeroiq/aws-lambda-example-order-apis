1a68959ffcdcc49bd62e7e139b14979915da5c16
zeeroiq <shrikantvishwakarma@outlook.com>
2023-10-23 00:24:57 +0530

    project restructured and clean up

--- lambda-order-apis/src/main/java/com/zeero/lambda/apis/CreateOrderAgent.java
+++ lambda-order-apis/src/main/java/com/zeero/lambda/apis/CreateOrderAgent.java
@@ -1,4 +1,9 @@
-package helloworld;
+package com.zeero.lambda.apis;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,15 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-
-/**
- * Handler for requests to Lambda function.
- */
-public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class CreateOrderAgent implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
@@ -39,9 +36,9 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         }
     }
 
-    private String getPageContents(String address) throws IOException{
+    private String getPageContents(String address) throws IOException {
         URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
     }
--- lambda-order-apis/src/test/java/com/zeero/lambda/AppTest.java
+++ lambda-order-apis/src/test/java/com/zeero/lambda/AppTest.java
@@ -1,15 +1,17 @@
-package helloworld;
+package com.zeero.lambda;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import com.zeero.lambda.apis.CreateOrderAgent;
 import org.junit.Test;
 
 public class AppTest {
   @Test
   public void successfulResponse() {
-    App app = new App();
+    CreateOrderAgent app = new CreateOrderAgent();
     APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
     assertEquals(200, result.getStatusCode().intValue());
     assertEquals("application/json", result.getHeaders().get("Content-Type"));
